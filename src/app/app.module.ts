1e13d76ae772b0932598c031da232d46a897e447
Ross <rossbaker32@gmail.com>
2020-08-19 21:55:59 +1000

    Add angular router and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ColoniesComponent } from './dashboard/colonies/colonies.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+import { NewColonyComponent } from './new-colony/new-colony.component';
+import { NewColonyFormComponent } from './new-colony/new-colony-form/new-colony-form.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'colonies', component: ColoniesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    ColoniesComponent,
+    NewColonyComponent,
+    NewColonyFormComponent,
   ],
   imports: [
     BrowserModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatTableModule,
+    MatDialogModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,14 +1,44 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { Colony } from './types';
+import gql from 'graphql-tag';
 
-const uri = 'http://localhost:8080/v1/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'http://localhost:8080/v1/graphql';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({uri}),
+    link: httpLink.create({ uri }),
     cache: new InMemoryCache(),
   };
+}
+
+export const ALL_COLONIES_QUERY = gql`
+  query AllColoniesQuery {
+    colonies {
+      id
+      hives
+      bees
+    }
+  }
+`;
+
+export interface AllColoniesQueryResponse {
+  colonies: Colony[];
+}
+
+export const CREATE_COLONY_MUTATION = gql`
+  mutation createBees($bees: Int!, $hives: Int!) {
+    insert_colonies(objects: { bees: $bees, hives: $hives }) {
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+export interface CreateColonyMutationResponse {
+  createColony: Colony;
 }
 
 @NgModule({
@@ -21,3 +51,6 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   ],
 })
 export class GraphQLModule {}
+
+// add queries in the component that needs it
+// add the database request as a service
