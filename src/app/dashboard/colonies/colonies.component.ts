ce7552b46ac1345b71e51b6b184f53dde2bbe2ee
Ross <rossbaker32@gmail.com>
2020-08-26 11:58:53 +1000

    Display upcoming collections on dashboard

--- src/app/dashboard/colonies/colonies.component.ts
+++ src/app/dashboard/colonies/colonies.component.ts
@@ -3,6 +3,7 @@ import { Colony } from 'src/app/types';
 import { MatTable } from '@angular/material/table';
 import { ColoniesService } from '../../colonies.service';
 import { Subscription } from 'rxjs';
+import { calculateOverproduction} from '../../../utils/helpers'
 import {
   Colonies,
   GetAllColoniesAndCollectionsQuery,
@@ -16,6 +17,8 @@ import {
 export class ColoniesComponent implements OnInit, OnDestroy {
   colonies: Colony[] = [];
   subscription: Subscription;
+  coloniesOverproduction = [];
+  loading = true;
 
   @ViewChild(MatTable) table: MatTable<any>;
 
@@ -25,16 +28,30 @@ export class ColoniesComponent implements OnInit, OnDestroy {
     'bees',
     'hives',
     'collectionDate',
+    'overproduction',
     'addCollection',
   ];
 
   constructor(private coloniesService: ColoniesService) {}
 
-  async ngOnInit() {
+  ngOnInit(): void {
+    console.log(this.loading);
+    
     this.subscription = this.coloniesService
       .getAllColoniesAndCollections()
       .valueChanges.subscribe((response) => {
+        
+        this.loading = response.loading; 
         this.colonies = (response.data as GetAllColoniesAndCollectionsQuery).colonies;
+        this.coloniesOverproduction = [];
+        this.colonies.forEach(colony => {
+          let overproduction = calculateOverproduction(colony)
+            this.coloniesOverproduction.push({
+              ...colony,
+              overproduction
+            })
+          })
+        this.table.renderRows();
       });
   }
 
